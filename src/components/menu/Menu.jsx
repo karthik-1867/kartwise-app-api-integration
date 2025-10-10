0b4b3bdadb2b2cbdb058f2d092d495aaa21e26c0
karthik-1867 <karthikv1867@gmail.com>
2025-10-10 12:22:23 +0530

    Update Menu.jsx

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -49,6 +49,63 @@ export default function Menu() {
             </div>
             </Link>
          </div>
+
+         <div className="MenuExpense">
+          <h1 className='title'>Expense dashboard</h1>
+          <Link to="/expenseSheet" style={{textDecoration:'none',color:'inherit'}}>
+          <div className="MenuItems">
+              <AttachMoneyIcon/>
+              <span>Create expense</span>
+          </div>
+          </Link>
+          <Link to="/expenseSummary" style={{textDecoration:'none',color:'inherit'}}>
+          <div className="MenuItems">
+            <PriceChange/>
+            <span>Edit Expense</span>
+          </div>
+          </Link>
+          <Link to="/acknowledge" style={{textDecoration:'none',color:'inherit'}}>
+          <div className="MenuItems">
+            <PlaylistAddCheckIcon/>
+            <span>Acknowledge</span>
+          </div>
+          </Link>
+          <Link to="/debts" style={{textDecoration:'none',color:'inherit'}}>
+          <div className="MenuItems">
+            <AnnouncementIcon />
+            <span>Debts</span>
+          </div>
+          </Link>
+          <Link to="/message" style={{textDecoration:'none',color:'inherit'}}>
+          <div className="MenuItems">
+            <Notifications />
+            <span>Notifications</span>
+          </div>
+          </Link>
+
+          <div className="MenuItems" onClick={handleSignout}>
+            <Logout/>
+            <span>Logout</span>
+          </div>
+
+         </div>
+
+         <div className="MenuExpense">
+          <h1 className='title'>Quick access</h1>
+          <div className="MenuItems">
+              <Avatar/>
+              <span>Ooty</span>
+          </div>
+          <div className="MenuItems">
+            <Avatar/>
+            <span>Manali</span>
+          </div>
+          <div className="MenuItems">
+            <Avatar/>
+            <span>Coimature</span>
+          </div>
+
+         </div>
          
          
       </div>
